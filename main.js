2aaf4495fdba0f869b74562518ffdeddff42b18b
Balsod <127954024+Balsod@users.noreply.github.com>
2023-05-31 15:33:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const fullname =document.getElementById("name")
-const image = document.getElementById("img")
+const fullname =document.getElementById("fullname")
+const image = document.getElementById("image")
 const title = document.getElementById("title")
-const phone = document.getElementById("phone")
+const phone = document.getElementById("phonenumber")
 const email = document.getElementById("email")
-const address = document.getElementById("address")
+const address = document.getElementById("streetaddress")
+const btn = document.getElementById("btn")
+const url = "https://randomuser.me/api/"
 
-const url = "http://randomuser.me/api/"
-
-const cardData = () => {
+function cardData(){
 
     fetch(url) 
         .then(response => {
@@ -16,11 +16,14 @@ const cardData = () => {
 
         })
         .then((data) => {
-            image.src =` ${data.results[0].image.medium}`;
-            fullname.textContent +=`${data.results[0].name.title}. ${data.results[0].name.first} ${data.results[0].name.second} ${data.results[0].name.last}`
+            console.log(data);
+            image.src =` ${data.results[0].picture.medium}`;
+            fullname.textContent +=`${data.results[0].name.first}  ${data.results[0].name.last}`
+            title.textContent = `${data.results[0].name.title}`
             phone.textContent += `${data.results[0].phone}`
             email.textContent += `${data.results[0].email}`
-            address.textContent+= `${data.results[0].address}`
+            address.textContent+= `${data.results[0].location.city}`
+           
 
             console.log(data.results[0].image.medium)
         });
@@ -29,5 +32,5 @@ const cardData = () => {
 
         };
 
-        cardData
+btn.addEventListener("click", cardData)
     
